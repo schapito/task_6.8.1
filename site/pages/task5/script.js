7643a52246616657bcfdbfdf6a307d214d0d4215
Yaroslav Yakovlev <ymyakovlev@gmail.com>
2022-11-01 17:21:32 +0300

    update script.js task 6

--- site/pages/task5/script.js
+++ site/pages/task5/script.js
@@ -1,16 +1,16 @@
 //Объявление трех переменных//
 const input = document.querySelector('input') //поле ввода//
 const button = document.querySelector('button') //кнопка//
-const duplicateField = document.querySelector('#duplicateField') // 
+const duplicateField = document.querySelector('#duplicateField') // поле ввода текста в <p> по селектору //
 
 
-input.addEventListener('input', function (event) { 
+input.addEventListener('input', function (event) { //вводим текст в поле //
     event.preventDefault();
-    duplicateField.textContent = input.value;
+    duplicateField.textContent = input.value; //текст дублируется в в блок duplicateField//
 })
 
 
-button.addEventListener('click', function (event) {
+button.addEventListener('click', function (event) { //при нажатии кнопки текст, написанный из input, появляется в консоли, поле ввода(input) и duplicateField очищаются//
     console.log(input.value)
     input.value = "";
     duplicateField.textContent = "";
--- site/pages/task6/script.js
+++ site/pages/task6/script.js
@@ -1,43 +1,43 @@
-const trafficLight1 = document.querySelector('#trafficLight1'); // id="trafficLight1"
-const trafficLight2 = document.querySelector('#trafficLight2'); // id="trafficLight2"
-const trafficLight3 = document.querySelector('#trafficLight3'); // id="trafficLight3"
+const trafficLight1 = document.querySelector('#trafficLight1'); //объявляю переменную trafficLight1 через поиск селектором trafficLight1//
+const trafficLight2 = document.querySelector('#trafficLight2'); //объявляю переменную trafficLight2 через поиск селектором trafficLight2//
+const trafficLight3 = document.querySelector('#trafficLight3'); //объявляю переменную trafficLight3 через поиск селектором trafficLight3//
 
-function makeGreen() {
-    trafficLight1.style.background = ('green'); // style.background - свойство элемента для смены цвета.
-    trafficLight2.style.background = ('black');
-    trafficLight3.style.background = ('black');
-    trafficLight1.removeEventListener('click', makeGreen);
-    trafficLight2.removeEventListener('click', makeGreen)
-    trafficLight3.removeEventListener('click', makeGreen)
+function makeGreen() { //объявляю функцию "makeGreen"
+    trafficLight1.style.background = ('green'); //переменная trafficLight1 со свойством style.background  задает цвет фона зеленым//
+    trafficLight2.style.background = ('black'); //переменная trafficLight2 со свойством style.background  задает цвет фона черным//
+    trafficLight3.style.background = ('black'); //переменная trafficLight3 со свойством style.background  задает цвет фона черным//
+    trafficLight1.removeEventListener('click', makeGreen); //следующий шаг - удалить событие по клику функцию "makeGreen"//
+    trafficLight2.removeEventListener('click', makeGreen);
+    trafficLight3.removeEventListener('click', makeGreen);
     trafficLight1.addEventListener('click', makeYellow);
     trafficLight2.addEventListener('click', makeYellow);
     trafficLight3.addEventListener('click', makeYellow);
 }
 
-function makeYellow() {
-    trafficLight1.style.background = ('black');
+function makeYellow() { //объявляю функцию "makeYellow"
+    trafficLight1.style.background = ('black'); 
     trafficLight2.style.background = ('yellow');
     trafficLight3.style.background = ('black');
     trafficLight1.removeEventListener('click', makeYellow);
-    trafficLight2.removeEventListener('click', makeYellow)
-    trafficLight3.removeEventListener('click', makeYellow)
+    trafficLight2.removeEventListener('click', makeYellow);
+    trafficLight3.removeEventListener('click', makeYellow);
     trafficLight1.addEventListener('click', makeRed);
     trafficLight2.addEventListener('click', makeRed);
     trafficLight3.addEventListener('click', makeRed);
 }
 
-function makeRed() {
+function makeRed() { //объявляю функцию "makeRed"
     trafficLight1.style.background = ('black');
     trafficLight2.style.background = ('black');
     trafficLight3.style.background = ('red');
     trafficLight1.removeEventListener('click', makeRed);
-    trafficLight2.removeEventListener('click', makeRed)
-    trafficLight3.removeEventListener('click', makeRed)
+    trafficLight2.removeEventListener('click', makeRed);
+    trafficLight3.removeEventListener('click', makeRed);
     trafficLight1.addEventListener('click', makeGreen);
     trafficLight2.addEventListener('click', makeGreen);
     trafficLight3.addEventListener('click', makeGreen);
 }
 
-trafficLight1.addEventListener('click', makeGreen);
-trafficLight2.addEventListener('click', makeYellow);
-trafficLight3.addEventListener('click', makeRed);
+trafficLight1.addEventListener('click', makeGreen); //запускаю функцию "makeGreen" по клику
+trafficLight2.addEventListener('click', makeYellow); //запускаю функцию "makeYellow" по клику
+trafficLight3.addEventListener('click', makeRed); //запускаю функцию "makeRed" по клику
